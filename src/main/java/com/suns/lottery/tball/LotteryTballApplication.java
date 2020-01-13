6533f856ddc450d9dd6e97e3762b7dbb83006c11
sunxiaobo7 <sunxiaobo@jd.com>
2020-01-13 11:52:58 +0800

    first commit

---
+++ src/main/java/com/suns/lottery/tball/LotteryTballApplication.java
@@ -0,0 +1,13 @@
+package com.suns.lottery.tball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryTballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotteryTballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suns/lottery/tball/LotteryTballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suns.lottery.tball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryTballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
